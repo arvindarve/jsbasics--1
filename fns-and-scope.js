0f12d2832ec7429df563f19cf97f05f50244fb62
arvindarve <arveeearvind7@gmail.com>
2017-09-12 17:05:13 -0400

    again started from first

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -21,11 +21,11 @@ var name = 'Tyler';
 
   //Code Here
   
-  // function getName(){
-  //   var name = prompt("whats your name");
-  //   return name;
-  // }
-  // getName();
+  function getName(){
+    var name = prompt("whats your name");
+    return name;
+  }
+  getName();
 
 
 //////////////////PROBLEM 3////////////////////
@@ -35,10 +35,10 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  // //Code Here
-  // function welcome() {
-  //   alert("Welcome, " + getName());
-  // }
+  //Code Here
+  function welcome() {
+    alert("Welcome, " + getName());
+  }
 
 //////////////////PROBLEM 4////////////////////
 
